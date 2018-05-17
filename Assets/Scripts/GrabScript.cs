4c9f9b2161b07b40d020b124f00e12c58d53cffd
DESKTOP-RT9K64F\Renz <lilfighter__@hotmail.com>
2018-05-17 15:18:17 -0600

    - May 17 Commit. I think I added matlab server to communicate with hd2

---
+++ Assets/Scripts/GrabScript.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GrabScript : MonoBehaviour {
+
+    public Vector3 acceleration;
+    public Vector3 velocity;
+    public Vector3 speed;
+    public float xForce, yForce;
+    public float collisionStatus;
+    public RoomAliveToolkit.RATUserViewCamera userCam;
+
+    private Vector3 mousePosScreen, currentPos, previousPos;
+    private Vector3 previousVel;
+    private Rigidbody carRb2d;
+    private Rigidbody characterRb2d;
+    private Vector3 robotPos;
+    private float T11 = 1, T12 = 0, T13 = 1, T21 = 0, T22 = 1, T23 = 1, T31 = 1, T32 = 1, T33 = 1;
+    private float a = 0, b = 0, c = 0, d = 0;
+    private bool forceFeedback;
+    private Transform characterTransform;
+
+    private void Start()
+    {
+        Calibrate();
+        characterRb2d = GetComponent<Rigidbody>();
+        characterTransform = GetComponent<Transform>();
+        robotPos = Vector3.zero;
+        previousPos = transform.position;
+        previousVel = characterRb2d.velocity;
+       
+    }
+
+    public void Calibrate() //Initialize
+    {
+        try
+        {
+            T11 = PlayerPrefs.GetFloat("T11");
+            T12 = PlayerPrefs.GetFloat("T12");
+            T13 = PlayerPrefs.GetFloat("T13");
+
+            T21 = PlayerPrefs.GetFloat("T21");
+            T22 = PlayerPrefs.GetFloat("T22");
+            T23 = PlayerPrefs.GetFloat("T23");
+
+            T31 = PlayerPrefs.GetFloat("T31");
+            T32 = PlayerPrefs.GetFloat("T32");
+            T33 = PlayerPrefs.GetFloat("T33");
+            /*
+            a = PlayerPrefs.GetFloat("a");
+            b = PlayerPrefs.GetFloat("b");
+            c = PlayerPrefs.GetFloat("c");
+            d = PlayerPrefs.GetFloat("d");*/
+        }
+        catch
+        {
+            Debug.Log("Please Calibrate");
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+		if (Input.GetKey(KeyCode.UpArrow)) //If using mouse to move 
+        {
+            characterRb2d.velocity = new Vector3(1,1,1);  
+        }
+        else if (MatlabServer.instance.serverRunning == true) //If connected to rehab robot
+        {
+            characterTransform.position = new Vector3(MatlabServer.instance.xMove, MatlabServer.instance.yMove, MatlabServer.instance.zMove);
+            //characterRb2d.velocity = (CalibratedMovement() - characterTransform.position) * 30;
+        }
+        else
+        {
+            //Do Nothing
+        }
+        velocity = (transform.position - previousPos) / Time.deltaTime;
+        acceleration = (characterRb2d.velocity - previousVel) / Time.deltaTime;
+        speed = new Vector3(Mathf.Abs(velocity.x), Mathf.Abs(velocity.y), 0);
+
+        previousPos = transform.position;
+        previousVel = characterRb2d.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 contactNormal = collision.contacts[0].normal.normalized; //Gives unit vector direction normal to point of contact
+
+
+        //Debug.Log("x: " +xForce + " y: "+ yForce + " Vel: "+ velocity + " UnityVel: "+characterRb2d.velocity+" Accel: " + acceleration);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //While in contact with ball
+        Vector3 contactNormal = collision.contacts[0].normal.normalized; //Gives unit vector direction normal to point of contact
+        if (forceFeedback)
+        {
+        }
+        else
+        {
+            xForce = 0;
+            yForce = 0;
+        }
+        MatlabServer.instance.collisionStatus = 1f;
+        MatlabServer.instance.xForce = xForce;
+        MatlabServer.instance.yForce = yForce;
+
+        //Debug.DrawRay(collision.contacts[0].point, contactNormal, Color.red, 2, true);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        xForce = 0;
+        yForce = 0;
+        MatlabServer.instance.collisionStatus = 0f;
+        MatlabServer.instance.xForce = xForce;
+        MatlabServer.instance.yForce = yForce;
+    }
+
+    /*public Vector3 CalibratedMovement()
+    {
+        // Ps = T*Pr
+        float xS = MatlabServer.instance.xMove * T11 + MatlabServer.instance.yMove * T12;
+        float yS = MatlabServer.instance.xMove * T21 + MatlabServer.instance.yMove * T22;
+        float lambda = MatlabServer.instance.xMove * T31 + MatlabServer.instance.yMove * T32 + T33;
+
+        
+        float xS = MatlabServer.instance.xMove * a - MatlabServer.instance.yMove * b + c;
+        float yS = MatlabServer.instance.xMove * b + MatlabServer.instance.yMove * a + d;
+        
+        return new Vector3(xS/lambda, yS/lambda, 0f);
+    }*/
+}
---
+++ Assets/Scripts/MatlabServer.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using RoomAliveToolkit;
+
+public class MatlabServer : MonoBehaviour {
+
+    public static MatlabServer instance;
+    public float xMove, yMove, zMove = 0;
+    public float xForce, yForce = 0;
+    public float collisionStatus = 0;
+    public float forceFeedback = 0;
+
+    [ReadOnly] public bool serverRunning = false;
+    [ReadOnly] public string ipAddress = "142.244.62.103"; //This comp: 142.244.63.45, Localhost: 127.0.0.1   
+    [ReadOnly] public int port = 9000; 
+    private Thread thread;
+    private Socket newsock;
+    private bool stop = false;
+    private bool threadRunning = false;
+    private int recvSize = 24;
+
+    // Use this for initialization
+    void Awake()
+    { //Always called before start() functions
+      //Makes sure that there is only one instance of Matlab Server (singleton)
+        if (instance == null) //If no game control found
+        {
+            instance = this; //Then this is the instance of the game control
+        }
+        else if (instance != this) //If the game object finds that instance is already on another game object, then this destroys itself as it's not needed
+        {
+            Destroy(gameObject);
+        }
+        //ipAddress = PlayerPrefs.GetString("IPAddress", "127.0.0.1");
+        forceFeedback = (float)PlayerPrefs.GetInt("ForceToggle", 1);
+    }
+
+    private void Start()
+    {
+        StartThread();
+    }
+
+    void OnApplicationQuit()
+    {
+        if (serverRunning || threadRunning)
+        {
+            Debug.Log("Quit~!");
+            StopThread();
+        }
+    }
+
+    public void StartThread()
+    {
+        thread = new Thread(new ThreadStart(ThreadMethod));
+        thread.Start();
+    }
+
+    public void StopThread()
+    {
+        if (serverRunning)
+        {
+            stop = true;
+        }
+        else
+        {
+            thread.Abort();
+        }
+        newsock.Close();
+    }
+
+    private void ThreadMethod()
+    {
+        threadRunning = true;
+        int recv;
+        byte[] dataRecv = new byte[recvSize];  //Data Received from Simulink
+        byte[] dataSend = new byte[32]; //Send Collision Status, X, Y
+        IEnumerable<byte> dataSendLINQ = new byte[32]; //Initialize LINQ for easy concatenation later for sending
+        //Create IP End point, where I want to connect (Local IP/Port)
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+        //Create UDP Socket
+        newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        //Bind to ip. Server waits for a client at specified ip & port. 
+        try
+        {
+            newsock.Bind(ipep);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Winsock Error: " + e.ToString());
+        }
+        Debug.Log("Connecting to IP: "+ ipAddress + " Port: "+ port +" Waiting for a client...");
+
+        //Get IP of client
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+        Debug.Log("1");
+        EndPoint Remote = (EndPoint)(sender);
+        Debug.Log("2");
+        //Receive binary Data from client
+        recv = newsock.ReceiveFrom(dataRecv, ref Remote);
+        Debug.Log("3");
+        //Decode data and display
+        serverRunning = true;
+        Debug.Log("Message received from " + Remote.ToString());
+
+        while (true)
+        {
+            //Receive X Y positions
+            dataRecv = new byte[recvSize];
+            recv = newsock.ReceiveFrom(dataRecv, ref Remote);
+
+
+            //Convert Bytes into Doubles (This gets referenced by mainPlayer.cs to move the character) X/Y Position
+            xMove = (float)BitConverter.ToDouble(dataRecv, 0); //x
+            yMove = (float)BitConverter.ToDouble(dataRecv, 8); //y
+            zMove = (float)BitConverter.ToDouble(dataRecv, 16); //z
+
+
+            //Debug.Log("X: " +  xMove + " Y: " + yMove + "Z: " + zMove); //Display X/Y Position
+
+            //Concatenate Collision Status, ForceFeedbackStatus, xForce, yForce. 
+            dataSendLINQ = (BitConverter.GetBytes((double)collisionStatus)).Concat(BitConverter.GetBytes((double)forceFeedback)).Concat(BitConverter.GetBytes((double)xForce)).Concat(BitConverter.GetBytes((double)yForce));
+            dataSend = dataSendLINQ.ToArray(); //Convert to byte Array from IEnumerable byte Array
+
+            //Debug.Log("X_f: " + xForce + " Y_f: " + yForce); //xForce, yForce
+
+            //Send Info to Simulink
+            newsock.SendTo(dataSend, dataSend.Length, SocketFlags.None, Remote);
+
+            if (stop)
+            {
+                break;
+            }
+
+            //Debug.Log(dataSend[0] + " " + dataSend[1] + " " + dataSend[2] + " " + dataSend[3] + " " + dataSend[4]); 
+        }
+        Debug.Log("Exiting Thread...");
+    }
+}
